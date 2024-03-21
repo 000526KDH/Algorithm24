8fe7d048b87363cff2cf3d910fb9a5651702849e
Name명 <leo502626@gmail.com>
2024-03-21 15:46:42 +0900

    ALGORITHM24

--- 1장/1장26번.py
+++ 1장/1장26번.py
@@ -1,13 +1,14 @@
-def is_proper_subset(A,B):
-    if set(A)==set(B):
-        return False
-    return set(A).issubset(set(B))
+def is_proper_subset(A, B):
+    # A와 B를 집합으로 변환
+    set_A = set(A)
+    set_B = set(B)
+    
+    # A가 B의 부분집합이고, A와 B가 동일하지 않으면 True 반환
+    return set_A.issubset(set_B) and set_A != set_B
 
-A=[1,2,3]
-B=[1,2,3,4,5]
-is_proper = is_proper_subset(A,B)
+# 예시 리스트
+A = [1, 2, 3, 4, 5]
+B = [4, 5, 6, 7, 8]
 
-if is_proper:
-    print("A는 B의 진부분집합입니다.")
-else:
-    print("A는 B의 진부분집합이 아닙니다.")
+# A와 B의 진부분집합 여부 확인
+print("A가 B의 진부분집합인가?:", is_proper_subset(A, B))

2e1bfefacfe9e220941f4d74af879677cc0e90f0
Name명 <leo502626@gmail.com>
2024-04-11 14:52:57 +0900

    main

--- 3장/AL3.1_selection_sort.ipynb/AL3.4.py
+++ 3장/AL3.1_selection_sort.ipynb/AL3.4.py
@@ -7,6 +7,7 @@ def closest_pair(p):
             if dist < mindist:
                 mindist = dist
     return mindist
+
 import math
 def distance(p1,p2):
     return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)

f40871592b36d0075f0ddc9408ec29b1f8d07e7d
Name명 <leo502626@gmail.com>
2024-04-16 13:32:50 +0900

    main

--- 3장/AL3.1_selection_sort.ipynb/AL3.4.py
+++ 3장/AL3.1_selection_sort.ipynb/AL3.4.py
@@ -1,16 +1,25 @@
+import math
+
+def distance(p1, p2):
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
 def closest_pair(p):
-    n=len(p)
-    mindist=float("inf")
+    minp1 = [0, 0]
+    minp2 = [0, 0]
+    n = len(p)
+    mindist = float("inf")
     for i in range(n-1):
-        for j in range(i+1,n):
-            dist = distance(p[i],p[j])
+        for j in range(i+1, n):
+            dist = distance(p[i], p[j])
             if dist < mindist:
                 mindist = dist
-    return mindist
+                minp1 = p[i]
+                minp2 = p[j]
+    return mindist, minp1, minp2
 
-import math
-def distance(p1,p2):
-    return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+p = [(2,3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]
 
-p=[(2,3),(12,30),(40,50),(5,1),(12,10),(3,4)]
-print("최근접 거리:",closest_pair(p))
+mindist, minp1, minp2 = closest_pair(p)
+print("최근접 거리:", mindist)
+print("첫번째 점의 값 :", minp1)
+print("두번쨰 점의 값: ", minp2)
